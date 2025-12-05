de6820be0a723a15b897d9c25388a7c5389ef0ba
lorickagl <Agnoly.lorick@gmail.com>
2025-12-05 14:49:02 +0100

    MàJ1 Php

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
+   <header>
+        <nav>
+            <ul>
+                <div class="nav-custom">
+                    <div>
+                        <li>
+                            <a href="">Page d'accueil</a>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <a href="">Page de contact</a>
+                        </li>
+                    </div>
+                </div>
+            </ul>
+        </nav>
+    </header>
     
 </body>
 </html>
